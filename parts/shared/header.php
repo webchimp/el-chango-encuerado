f44f290d4830f799f0f5a3029fb201c0e98a845c
Rodrigo Tejero <rodrigo.tejero@thewebchi.mp>
2012-11-21 08:38:26 -0600

    Cambio de Header
    Se le cambió la liga del logo para que mande al site_url()

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -1,5 +1,4 @@
 
 <header>
-	<h1 class="logo"><a href="/"><?php bloginfo( 'name' ); ?></a></h1>
-	<?php //get_search_form(); ?>
+	<h1 class="logo"><a href="<?php echo site_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
 </header>
