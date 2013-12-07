8d88ef5ffb49cea972e66bfeb3034a577cbb725c
Rodrigo Tejero <rodrigo.tejero@thewebchi.mp>
2013-12-07 12:44:33 -0600

    Favicons
    Se agregaron los favicons de todos tipos

--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -1,16 +1,26 @@
 <!DOCTYPE HTML>
-<!--[if IEMobile 7 ]><html class="no-js iem7" manifest="default.appcache?v=1"><![endif]--> 
-<!--[if lt IE 7 ]><html class="no-js ie6" lang="en"><![endif]--> 
-<!--[if IE 7 ]><html class="no-js ie7" lang="en"><![endif]--> 
-<!--[if IE 8 ]><html class="no-js ie8" lang="en"><![endif]--> 
+<!--[if IEMobile 7 ]><html class="no-js iem7" manifest="default.appcache?v=1"><![endif]-->
+<!--[if lt IE 7 ]><html class="no-js ie6" lang="en"><![endif]-->
+<!--[if IE 7 ]><html class="no-js ie7" lang="en"><![endif]-->
+<!--[if IE 8 ]><html class="no-js ie8" lang="en"><![endif]-->
 <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" lang="en"><!--<![endif]-->
 	<head>
-		<title><?php bloginfo( 'name' ); ?><?php wp_title( '|' ); ?></title>
-		<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title><?php bloginfo('name'); ?><?php wp_title( '|' ); ?></title>
+		<meta charset="<?php bloginfo('charset'); ?>" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"><!-- Remove if you're not building a responsive site. (But then why would you do such a thing?) -->
-		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+		
+		<link rel="icon" type="image/x-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico" />
+		<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.png" />
+		
+		<!-- For iPhone 4 Retina display: -->
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_stylesheet_directory_uri(); ?>/img/apple-touch-icon-114x114-precomposed.png">
+		<!-- For iPad: -->
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_stylesheet_directory_uri(); ?>/img/apple-touch-icon-72x72-precomposed.png">
+		<!-- For iPhone: -->
+		<link rel="apple-touch-icon-precomposed" href="<?php echo get_stylesheet_directory_uri(); ?>/img/apple-touch-icon-57x57-precomposed.png">
+		
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
