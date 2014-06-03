e38dae8b7389c5f2b4211a1941c13f809b0c96d3
webchimp <rodrigo.tejero@thewebchi.mp>
2014-06-02 22:44:08 -0500

    Added CDN JS, CSS and removed non-CDN versions

--- functions.php
+++ functions.php
@@ -49,23 +49,26 @@
 			wp_enqueue_script('google.maps');
 		endif;
 
-		wp_register_script('modernizr', get_template_directory_uri() . '/js/modernizr.js');
-		//wp_enqueue_script('modernizr');
+		wp_register_script('modernizr', '//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.min.js');
+		wp_enqueue_script('modernizr');
+
+		wp_register_script('html5shiv', '//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.js');
+		wp_enqueue_script('html5shiv');
+
+		wp_register_script('jquery.cycle.all.min', '//cdnjs.cloudflare.com/ajax/libs/jquery.cycle/3.03/jquery.cycle.all.min.js', array('jquery'));
+		wp_enqueue_script('jquery.cycle.all.min');
+
+		//wp_register_script('jquery.cyclewrap', get_template_directory_uri() . '/js/jquery.cyclewrap.min.js', array('jquery.cycle.all.min'));
+		//wp_enqueue_script('jquery.cyclewrap');
+
+		wp_register_script('jquery.magnific-popup.min', '//cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/0.9.9/jquery.magnific-popup.min.js', array('jquery'));
+		wp_enqueue_script('jquery.magnific-popup.min');
+
+		//wp_register_script('jquery.slimscroll.min', '//cdnjs.cloudflare.com/ajax/libs/jQuery-slimScroll/1.3.1/jquery.slimscroll.min.js', array('jquery'));
+		//wp_enqueue_script('jquery.slimscroll.min');
 
 		wp_register_script('jquery.validator2', get_template_directory_uri() . '/js/jquery.validator2.js', array('jquery'));
 		wp_enqueue_script('jquery.validator2');
-
-		wp_register_script('jquery.cycle', get_template_directory_uri() . '/js/jquery.cycle.all.js', array('jquery'));
-		wp_enqueue_script('jquery.cycle');
-
-		//wp_register_script('jquery.cyclewrap', get_template_directory_uri() . '/js/jquery.cyclewrap.min.js', array('jquery.cycle'));
-		//wp_enqueue_script('jquery.cyclewrap');
-
-		wp_register_script('jquery.magnific-popup.min', get_template_directory_uri() . '/js/jquery.magnific-popup.min.js', array('jquery'));
-		wp_enqueue_script('jquery.magnific-popup.min');
-
-		//wp_register_script('jquery.slimscroll.min', get_template_directory_uri() . '/js/jquery.slimscroll.min.js', array('jquery'));
-		//wp_enqueue_script('jquery.slimscroll.min');
 
 		wp_register_script('site', get_template_directory_uri() . '/js/site.js', array('jquery', 'jquery-form', 'jquery-color'));
 		wp_enqueue_script('site');
@@ -74,7 +77,7 @@
 		wp_register_style('screen', get_template_directory_uri() . '/style.css', '', '', 'screen');
 		wp_enqueue_style('screen');
 
-		wp_register_style('magnific-popup', get_template_directory_uri() . '/css/magnific-popup.css', '', '', 'screen');
+		wp_register_style('magnific-popup', '//cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/0.9.9/magnific-popup.css', '', '', 'screen');
 		wp_enqueue_style('magnific-popup');
 
 		wp_register_style('site', get_template_directory_uri() . '/site.css', '', '', 'screen');
--- js/site.js
+++ js/site.js
@@ -1,26 +1,5 @@
-document.createElement('header');
-document.createElement('hgroup');
-document.createElement('nav');
-document.createElement('menu');
-document.createElement('section');
-document.createElement('article');
-document.createElement('aside');
-document.createElement('footer');
-
 jQuery(document).ready(function($) {
-	
+
 	// Your JavaScript goes here
-	
-	
-	//Contacto -------------------------------------------------------------------------------------
-	var forma_contacto_options = {
-		url:		ajaxurl,
-		data:		{ action: 'formulario_contacto' },
-		success:	function(responseText, statusText, xhr, $form){
-						alert(responseText);
-					}
-	};
-	
-	$('#forma-contacto').ajaxForm(forma_contacto_options);
 });
 
